bdce3bb727e2316f31c593a1c79bf43ed2085ef3
guesswhozzz <torridtime@gmail.com>
2020-11-08 13:40:47 +0300

    Refactor term color palette printer

--- scheme.sh
+++ scheme.sh
@@ -3,17 +3,13 @@
 function palettePrinter() {
   local acc=$(($1 - 1))
   local CSI="\x1b["
-  local RST="${CSI}0m"
   local n=$acc
-  local prefix="Term scheme:"
-
   if [ $acc -ne -1 ]; then
-    echo "$prefix ${CSI}1;3${n}mnormal  ${RST}${CSI}4${n}m     ${RST}"
-    echo "$prefix ${CSI}1;9${n}mbright  ${RST}${CSI}10${n}m     ${RST}"
-    palettePrinter acc
+    echo "Term scheme: ${CSI}${2}${n}m ${4}  ${CSI}0m${CSI}${3}${n}m     ${CSI}0m"
+    palettePrinter acc $2 $3 $4
   else
     return
   fi
 }
-
-palettePrinter 8
+palettePrinter 8 3 4 normal
+palettePrinter 8 9 10 bright
