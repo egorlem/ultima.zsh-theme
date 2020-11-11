045104ac2b87105b1dfe629ee48af447be71bc59
guesswhozzz <torridtime@gmail.com>
2020-11-11 18:36:18 +0300

    Add 256 colors printer

--- scheme.sh
+++ scheme.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 # Display base term color scheme
-function palettePrinter() {
+function termPalettePrinter() {
   local acc=$(($1 - 1))
   local CSI="\x1b["
   local n=$acc
   if [ $acc -ne -1 ]; then
     echo "Term scheme: ${CSI}${2}${n}m ${4}  ${CSI}0m${CSI}${3}${n}m     ${CSI}0m"
-    palettePrinter acc $2 $3 $4
+    termPalettePrinter acc $2 $3 $4
   else
     return
   fi
 }
-palettePrinter 8 3 4 normal
-palettePrinter 8 9 10 bright
+termPalettePrinter 8 3 4 normal
+termPalettePrinter 8 9 10 bright
+
+function fullPalettePrinter() {
+  local acc=$(($1 - 1))
+  local CSI="\x1b["
+  local n=$acc
+  if [ $acc -ne -1 ]; then
+    echo "256 colors: ${CSI}${2}${n}m ${n} ${CSI}0m${CSI}${3}${n}m     ${CSI}0m"
+    fullPalettePrinter acc $2 $3 $4
+  else
+    return
+  fi
+}
+fullPalettePrinter 256 "38;05;" "48;05;"
